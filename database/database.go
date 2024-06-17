da666be50cd7fc8d64056002fbb483189524d4a8
mctlmk <minkyu.lee@namutech.co.kr>
2024-06-17 13:50:37 +0900

    update query

--- database/database.go
+++ database/database.go
@@ -21,6 +21,23 @@ func ConnectDB(dsn string) (*sql.DB, error) {
 func FetchNewIssues(db *sql.DB, lastChecked time.Time) ([]model.Issue, error) {
 	formattedTime := lastChecked.Format("2006-01-02 15:04:05")
 	query := "SELECT id, subject, description, created_on, updated_on FROM bitnami_redmine.issues WHERE updated_on > ?"
+
+	// -- 일감 조회 / note 포함
+	//select i.id, is2.name, u.firstname, u.lastname, i.start_date, i.due_date, i.done_ratio, i.estimated_hours,
+	//(select e.name from bitnami_redmine.enumerations e where e.type = "IssuePriority" and i.priority_id = e.id) as priority,
+	//(select b.firstname from bitnami_redmine.users b where i.author_id = b.id) as author,
+	//i.subject, i.description,
+	//(select b.firstname from bitnami_redmine.users b where j.user_id = b.id) as commentor,
+	//j.notes, j.created_on
+	//  from bitnami_redmine.issues i,
+	//  	bitnami_redmine.issue_statuses is2,
+	//  	bitnami_redmine.users u,
+	//  	bitnami_redmine.journals j
+	// where i.status_id = is2.id
+	//   and i.assigned_to_id = u.id
+	//   and i.id = j.journalized_id
+	//order by j.created_on desc;
+
 	rows, err := db.Query(query, formattedTime)
 	if err != nil {
 		return nil, err
