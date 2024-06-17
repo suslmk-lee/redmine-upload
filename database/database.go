4cf0fd8e5f6288c89fde9fe62b17edb41b53dcb7
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-06-17 13:57:47 +0900

    update sql

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"redmine-upload/model"
 	"time"
 
@@ -20,23 +21,19 @@ func ConnectDB(dsn string) (*sql.DB, error) {
 // FetchNewIssues fetches new issues from the MySQL database
 func FetchNewIssues(db *sql.DB, lastChecked time.Time) ([]model.Issue, error) {
 	formattedTime := lastChecked.Format("2006-01-02 15:04:05")
-	query := "SELECT id, subject, description, created_on, updated_on FROM bitnami_redmine.issues WHERE updated_on > ?"
-
-	// -- 일감 조회 / note 포함
-	//select i.id, is2.name, u.firstname, u.lastname, i.start_date, i.due_date, i.done_ratio, i.estimated_hours,
-	//(select e.name from bitnami_redmine.enumerations e where e.type = "IssuePriority" and i.priority_id = e.id) as priority,
-	//(select b.firstname from bitnami_redmine.users b where i.author_id = b.id) as author,
-	//i.subject, i.description,
-	//(select b.firstname from bitnami_redmine.users b where j.user_id = b.id) as commentor,
-	//j.notes, j.created_on
-	//  from bitnami_redmine.issues i,
-	//  	bitnami_redmine.issue_statuses is2,
-	//  	bitnami_redmine.users u,
-	//  	bitnami_redmine.journals j
-	// where i.status_id = is2.id
-	//   and i.assigned_to_id = u.id
-	//   and i.id = j.journalized_id
-	//order by j.created_on desc;
+	query := `
+        SELECT i.id, is2.name, u.firstname, u.lastname, i.start_date, i.due_date, i.done_ratio, i.estimated_hours,
+        (SELECT e.name FROM bitnami_redmine.enumerations e WHERE e.type = 'IssuePriority' AND i.priority_id = e.id) AS priority,
+        (SELECT b.firstname FROM bitnami_redmine.users b WHERE i.author_id = b.id) AS author,
+        i.subject, i.description,
+        (SELECT b.firstname FROM bitnami_redmine.users b WHERE j.user_id = b.id) AS commentor,
+        j.notes, j.created_on
+        FROM bitnami_redmine.issues i
+        JOIN bitnami_redmine.issue_statuses is2 ON i.status_id = is2.id
+        JOIN bitnami_redmine.users u ON i.assigned_to_id = u.id
+        JOIN bitnami_redmine.journals j ON i.id = j.journalized_id
+        WHERE j.created_on > ?
+        ORDER BY j.created_on DESC`
 
 	rows, err := db.Query(query, formattedTime)
 	if err != nil {
@@ -47,9 +44,16 @@ func FetchNewIssues(db *sql.DB, lastChecked time.Time) ([]model.Issue, error) {
 	var issues []model.Issue
 	for rows.Next() {
 		var issue model.Issue
-		if err := rows.Scan(&issue.ID, &issue.Subject, &issue.Description, &issue.CreatedOn, &issue.UpdatedOn); err != nil {
+		var assigneeFirstName, assigneeLastName, commentorFirstName sql.NullString
+		if err := rows.Scan(
+			&issue.ID, &issue.Status, &assigneeFirstName, &assigneeLastName, &issue.StartDate, &issue.DueDate,
+			&issue.DoneRatio, &issue.EstimatedHours, &issue.Priority, &issue.Author, &issue.Subject,
+			&issue.Description, &commentorFirstName, &issue.Notes, &issue.CreatedOn,
+		); err != nil {
 			return nil, err
 		}
+		issue.Assignee = fmt.Sprintf("%s %s", assigneeFirstName.String, assigneeLastName.String)
+		issue.Commentor = commentorFirstName.String
 		issues = append(issues, issue)
 	}
 	return issues, nil
--- model/model.go
+++ model/model.go
@@ -4,9 +4,18 @@ import "time"
 
 // Issue represents an issue in the Redmine system
 type Issue struct {
-	ID          int       `json:"id"`
-	Subject     string    `json:"subject"`
-	Description string    `json:"description"`
-	CreatedOn   time.Time `json:"created_on"`
-	UpdatedOn   time.Time `json:"updated_on"`
+	ID             int       `json:"id"`
+	Status         string    `json:"status"`
+	Assignee       string    `json:"assignee"`
+	StartDate      time.Time `json:"start_date"`
+	DueDate        time.Time `json:"due_date"`
+	DoneRatio      int       `json:"done_ratio"`
+	EstimatedHours float64   `json:"estimated_hours"`
+	Priority       string    `json:"priority"`
+	Author         string    `json:"author"`
+	Subject        string    `json:"subject"`
+	Description    string    `json:"description"`
+	Commentor      string    `json:"commentor"`
+	Notes          string    `json:"notes"`
+	CreatedOn      time.Time `json:"created_on"`
 }
